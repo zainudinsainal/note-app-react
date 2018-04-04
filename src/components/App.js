512425d0af11708faacf88728e02543f5e2fb448
zainudinsainal <supernova37@gmail.com>
2018-04-05 07:12:09 +0800

    Added home page

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Link, Route } from 'react-router-dom';
 import Dashboard from './Dashboard';
+import Home from './Home';
 
 class App extends React.Component {
   render() {
     return(
-      <Dashboard />
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/dashboard">Dashboard</Link>
+
+        <Route exact path="/" component={Home}/>
+        <Route path="/dashboard" component={Dashboard}/>
+      </div>
     )
   }
 }
